4416719c761f4b8da53b98e3a920292a3017ebec
kaliban <margiottasamuel533@gmail.com>
2024-07-10 23:55:29 +0200

    Changes to be committed: 	new file:   .gitignore 	new file:   clients.py 	new file:   main.py

---
+++ clients.py
@@ -0,0 +1,33 @@
+import threading
+import socket
+
+nickname = input("Choose a nickcname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 55555))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode("ascii")
+            if message == "NICK":
+                client.send(nickname.encode("ascii"))
+            else:
+                print(message)
+        except:
+            print(f"An error occurred!")
+            client.close()
+            break
+
+
+def write():
+    while True:
+        message = f"{nickname}: {input("")}"
+        client.send(message.encode("ascii"))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
